b4be9b6670338f8481b383eeee3cf2627fff6fef
sumit kumar <sumitkumar@sumits-MacBook-Air.local>
2024-04-08 17:55:19 +0530

    add: initial files

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import AddTodo from "./app/components/AddTodo"
+import Todos from "./app/components/Todos"
+
+
+function App() {
+
+  return (
+    <>
+    <h1>Learn about redux toolkit</h1>
+   <AddTodo/>
+   <Todos/>
+    </>
+  )
+}
+
+export default App
